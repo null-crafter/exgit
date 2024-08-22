f51b99efe096c90a4edf1e28f999b8f985d740bc
null crafter <163591881+null-crafter@users.noreply.github.com>
2024-08-22 23:08:16 +0000

    remove depth limits

--- lib/exgit/util/git.ex
+++ lib/exgit/util/git.ex
@@ -8,10 +8,8 @@ defmodule Exgit.Util.Git do
     Path.join(paths)
     |> Path.expand
   end
-  def ls_git_dirs(path, depth \\ 0, max_depth \\ 4) do
+  def ls_git_dirs(path) do
     cond do
-      depth >= max_depth ->
-        []
       git?(path) ->
         [path]
       true ->
@@ -19,7 +17,7 @@ defmodule Exgit.Util.Git do
         |> File.ls!()
         |> Enum.map(&Path.join([path, &1]))
         |> Enum.filter(&File.dir?/1)
-        |> Enum.map(&ls_git_dirs(&1, depth+1))
+        |> Enum.map(&ls_git_dirs/1)
         |> List.insert_at(0, path)
         |> List.flatten
         |> Enum.filter(&git?/1)
