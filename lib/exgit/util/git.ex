7e0c20685bdfa61731b16c55cbbd0671a551a7d1
null crafter <163591881+null-crafter@users.noreply.github.com>
2024-08-22 22:50:40 +0000

    much faster git dir detection

--- lib/exgit/util/git.ex
+++ lib/exgit/util/git.ex
@@ -1,55 +1,25 @@
 defmodule Exgit.Util.Git do
-  def shell_run(script, shell \\ "/bin/sh") do
-    System.cmd(shell, ["-c", script], stderr_to_stdout: true)
-  end
-  def git_toplevel_cmd(path) do
-      ~s(cd #{path}
-      && [ "`pwd`" = "`git rev-parse --show-toplevel`" ])
-      |> String.replace("\n", "")
-      |> String.replace("\r\n", "")
-      |> shell_run
-  end
-  def in_git_repo_cmd(path) do
-      ~s(cd #{path}
-      && git status)
-      |> String.replace("\n", "")
-      |> String.replace("\r\n", "")
-      |> shell_run
-  end
-
   def git?(path) do
-    case git_toplevel_cmd(path) do
-      {_, 0} ->
-        true
-      _ ->
-        false
-    end
-  end
-  def git_file?(path) do
-    case in_git_repo_cmd(path) do
-      {_, 0} ->
-        true
-      _ ->
-        false
-    end
+    File.ls!(path)
+    |> Enum.any?(&Enum.member?([".git", "packed-refs"], &1))
   end
 
   def path_join_expand(paths) do
     Path.join(paths)
     |> Path.expand
   end
-  def ls_git_dirs(path) do
+  def ls_git_dirs(path, depth \\ 0, max_depth \\ 4) do
     cond do
+      depth >= max_depth ->
+        []
       git?(path) ->
         [path]
-      git_file?(path) ->
-        []
       true ->
         path
         |> File.ls!()
         |> Enum.map(&Path.join([path, &1]))
         |> Enum.filter(&File.dir?/1)
-        |> Enum.map(&ls_git_dirs/1)
+        |> Enum.map(&ls_git_dirs(&1, depth+1))
         |> List.insert_at(0, path)
         |> List.flatten
         |> Enum.filter(&git?/1)
